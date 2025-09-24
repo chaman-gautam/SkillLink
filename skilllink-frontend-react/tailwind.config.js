38f46332297dc61c8305fc5598b4f5c81ef9c232
chaman-gautam <chaman12gautam12@gmai.com>
2025-09-24 18:56:09 +0530

    update frontend setup

--- skilllink-frontend-react/tailwind.config.js
+++ skilllink-frontend-react/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // <- tells Tailwind to scan your React files
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
